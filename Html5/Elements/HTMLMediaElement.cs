e604a27f31daae2cb1facd3896f419686f06ebc9
Michael Cheers <michaelcheers8@gmail.com>
2017-06-10 20:55:14 +0200

    Made it compile.

--- Html5/Elements/HTMLMediaElement.cs
+++ Html5/Elements/HTMLMediaElement.cs
@@ -7,7 +7,8 @@ namespace Bridge.Html5
     /// <typeparam name="TCurrentTarget">The CurrentTarget type of all MediaElement's events</typeparam>
     [External]
     [Name("HTMLMediaElement")]
-    public abstract \
+    [IgnoreGeneric]
+    public abstract class HTMLMediaElement<TCurrentTarget> : HTMLElement<TCurrentTarget> where TCurrentTarget : HTMLElement<TCurrentTarget>
     {
         /// <summary>
         /// Reflects the autoplay HTML attribute, indicating whether playback should automatically begin as soon as enough media is available to do so without interruption.
